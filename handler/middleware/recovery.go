68b4731878aa12f7c22b38a32d75fd271874ef14
kento <k.kent8192.dev@gmail.com>
2024-04-11 20:26:17 +0900

    WIP: Station 02 (Not Tested)

--- handler/middleware/recovery.go
+++ handler/middleware/recovery.go
@@ -1,15 +1,22 @@
 package middleware
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+)
 
 func Recovery(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
+		type userAgentContextKey string
 		defer func() {
 			if err := recover(); err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
 				w.Write([]byte("Internal Server Error"))
 			}
 		}()
+		k := userAgentContextKey("User-Agent")
+		ctx := context.WithValue(context.Background(), k, r.UserAgent())
+		r = r.WithContext(ctx)
 		h.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
